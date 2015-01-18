9b8b17c053cfc320ba0d13a39457f19052a9deaa
bugsancho <bugsancho@gmail.com>
2015-01-18 15:49:10 +0200

    Modified the ObjectTemplate.cshtml to take into account all the property meta data. Extracted logic from the template to a helper class.

---
+++ Web/RoofSharing.Web.Infrastructure/Helpers/DisplayForModelHelpers.cs
@@ -0,0 +1,23 @@
+﻿namespace RoofSharing.Web.Infrastructure.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    public static class DisplayForModelHelpers
+    {
+        public static bool ShouldShow(ModelMetadata metadata, ViewDataDictionary viewData)
+        {
+            return metadata.ShowForDisplay &&
+                   metadata.ModelType != typeof(System.Data.Entity.EntityState) &&
+                   !metadata.IsComplexType &&
+                   !viewData.TemplateInfo.Visited(metadata);
+        }
+
+        public static IEnumerable<ModelMetadata> GetPropertiesToDisplay(ViewDataDictionary viewData)
+        {
+            return viewData.ModelMetadata.Properties.Where(prop => ShouldShow(prop, viewData));
+        }
+    }
+}
